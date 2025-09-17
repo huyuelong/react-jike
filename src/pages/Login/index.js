6f4d996695598546106e956cae416535445d4a74
huyuelong <949910047@qq.com>
2025-09-18 02:14:23 +0800

    登录-使用redux维护token数据

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,18 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import logo from '@/assets/logo.png'
+import { useDispatch } from 'react-redux'
+import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const onFinish = (values) => {
         console.log(values)
+        dispatch(fetchLogin(values))
+        navigate('/')
+        message.success('登录成功')
     }
     return (
         <div className="login">
---
+++ src/store/modules/user.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { request } from '@/utils'
+
+const userStore = createSlice({
+    name: 'user',
+    // 数据状态
+    initialState: {
+        token: ''
+    },
+    // 同步修改方法
+    reducers: {
+        setToken: (state, action) => {
+            state.token = action.payload
+        }
+    }
+})
+
+const { setToken } = userStore.actions
+
+const userReducer = userStore.reducer
+
+// 异步方法 完成登录获取token
+const fetchLogin = (loginForm) => {
+    return async (dispatch) => {
+        const res = await request.post('/authorizations', loginForm)
+        dispatch(setToken(res.data.token))
+    }
+}
+
+export { setToken, fetchLogin }
+
+export default userReducer
