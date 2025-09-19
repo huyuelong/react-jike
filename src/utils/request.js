ebfc614a4ea1c518fac1bf174bad100f41c54cf7
huyuelong <949910047@qq.com>
2025-09-20 01:51:36 +0800

    处理token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { getToken } from './token'
+import { clearToken, getToken } from './token'
+import router from '@/router'
 
 const request = axios.create({
     baseURL: 'http://geek.itheima.net/v1_0',
@@ -26,6 +27,12 @@ request.interceptors.response.use((response) => {
 }, (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    // 401 未授权
+    if (error.response.status === 401) {
+        clearToken()
+        router.navigate('/login')
+        window.location.reload()
+    }
     return Promise.reject(error)
 })
 
