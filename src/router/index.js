25f04e09036ad4c8cbb5bcc1597e672bc0060b44
huyuelong <949910047@qq.com>
2025-09-20 23:07:10 +0800

    配置路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
+// import Home from "@/pages/Home";
+// import Article from "@/pages/Article";
+// import Publish from "@/pages/Publish";
+
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 const router = createBrowserRouter([
     {
@@ -13,15 +19,15 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Home />
+                element: <Suspense fallback={<div>加载中...</div>}><Home /></Suspense>
             },
             {
                 path: "Article",
-                element: <Article />
+                element: <Suspense fallback={<div>加载中...</div>}><Article /></Suspense>
             },
             {
                 path: "publish",
-                element: <Publish />
+                element: <Suspense fallback={<div>加载中...</div>}><Publish /></Suspense>
             },
         ]
     },
